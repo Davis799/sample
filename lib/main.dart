26622bf7e97bc848691689db457706291fa2268a
Davis799 <davistibz@gmail.com>
2022-09-15 10:37:14 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'screens/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Login",
+    home: login(),
+  ));
+}
---
+++ lib/screens/login.dart
@@ -0,0 +1,170 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class login extends StatefulWidget {
+  const login({super.key});
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  bool invisible = true;
+  void togglePass() {
+    setState(() {
+      invisible = !invisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 251, 249, 249),
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          SvgPicture.asset("assets/office.svg", height: 280, width: 600),
+          Padding(
+              padding: EdgeInsets.only(left: 17, bottom: 10),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Login",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 37,
+                    ),
+                  ))),
+          Padding(
+              padding: EdgeInsets.only(left: 17, bottom: 10, right: 17),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email_rounded),
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 195, 194, 194))),
+                    border: InputBorder.none,
+                    labelText: 'Email ID',
+                    hintText: 'Enter Your Email'),
+                style: TextStyle(
+                    fontSize: 17, color: Color.fromARGB(255, 0, 0, 0)),
+              )),
+          Padding(
+              padding: EdgeInsets.only(left: 17, right: 17),
+              child: TextFormField(
+                obscureText: invisible,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock_rounded),
+                  suffixIcon: GestureDetector(
+                      onTap: togglePass,
+                      child: Icon(
+                        invisible
+                            ? Icons.visibility_off
+                            : Icons.visibility_sharp,
+                      )),
+                  enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 195, 194, 194))),
+                  border: InputBorder.none,
+                  labelText: 'Password',
+                  hintText: 'Password',
+                ),
+                style: TextStyle(
+                    fontSize: 17, color: Color.fromARGB(255, 0, 0, 0)),
+              )),
+          Padding(
+              padding: EdgeInsets.only(right: 17, top: 20, bottom: 20),
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Forgot Password?",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Color.fromRGBO(1, 101, 255, 1)),
+                  ))),
+          TextButton(
+            // ignore: sort_child_properties_last
+            child: Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 17.0,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            style: TextButton.styleFrom(
+                padding: EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                minimumSize: Size(350, 50),
+                backgroundColor: Color.fromRGBO(1, 101, 255, 1),
+                alignment: Alignment.center),
+            onPressed: () {},
+          ),
+          Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: Text(
+                "OR",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                    color: Color.fromARGB(255, 135, 135, 135)),
+              )),
+          Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: TextButton(
+                // ignore: sort_child_properties_last
+                child: Flex(
+                    direction: Axis.horizontal,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/google.svg",
+                        width: 25,
+                        height: 25,
+                      ),
+                      Text(
+                        '  Login with Google',
+                        style: TextStyle(
+                          fontSize: 17.0,
+                          color: Color.fromARGB(255, 97, 97, 97),
+                        ),
+                      ),
+                    ]),
+                style: TextButton.styleFrom(
+                    padding: EdgeInsets.all(13),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    minimumSize: Size(350, 50),
+                    backgroundColor: Color.fromARGB(255, 222, 222, 222),
+                    alignment: Alignment.center),
+                onPressed: () {},
+              )),
+          Padding(
+              padding: EdgeInsets.only(top: 33, bottom: 10),
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Text.rich(TextSpan(style: TextStyle(),
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        TextSpan(
+                            text: "New to SkyConnect?",
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Color.fromARGB(255, 117, 123, 132))),
+                        TextSpan(
+                            text: "  Register",
+                            style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(1, 101, 255, 1)))
+                      ])))),
+        ],
+      )),
+    );
+  }
+}
